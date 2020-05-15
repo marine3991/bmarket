898efb0accd75d45d6a06c885024edc388573903
perth330 <perth330@hotmail.com>
2020-05-15 10:17:32 +0900

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name,:first_name,:family_name_kana,:first_name_kana,:nickname,:birthday])
   end
